6bf7a9a5423217b785a65bc42e5069e2a754ce41
Zachary Mueller <ZachAlanMueller@users.noreply.github.com>
2015-03-25 22:02:58 -0500

    Update tellme.py

--- tellme.py
+++ tellme.py
@@ -1,2 +1,9 @@
-def tell():
-    print "tellme importing works"
+def possible_exit():
+    print "Possible exit requested. Type 'confirm' to continue."
+    end_game = raw_input("> ")
+    end_game.lower()
+    if end_game == "confirm":
+    	print "\nExiting Game.\n"
+        exit(1)
+    else:
+        print "Continuing game..."
